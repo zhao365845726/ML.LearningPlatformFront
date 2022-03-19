51783aeca18af7f8525c14f07aa9bd852fe103d5
zhao365845726@163.com <zhao365845726@163.com>
2022-03-19 17:36:04 +0800

    修改米考云的连接接口地址

--- assets/js/common/config.js
+++ assets/js/common/config.js
@@ -1,6 +1,6 @@
 // var api_domain = 'http://mlearning.api.milisx.com/';
 // var api_domain = 'http://localhost:52118/';
-var api_domain = 'http://kxny.learn-api.milisx.com/';
+var api_domain = 'http://mkcloud.api.milisx.com/';
 
 function ajaxUrl(){ //导航列表url
     var url = api_domain + 'api/content/v1/';
