f13177ee5348cb45cca7b38ae64b1c104455d05a
Asuna <2471933886@qq.com>
2022-05-28 08:57:45 +0800

    merge

--- assets/js/common/config.js
+++ assets/js/common/config.js
@@ -1,13 +1,13 @@
-// var api_domain = 'http://milisx.tpddns.cn:82/';
-// var api_domain = 'http://mkcloud.api.milisx.com/';
-var api_domain = 'https://kxny.learn-dev.milisx.com/';
+// var api_domain = 'http://mlearning.api.milisx.com/';
+// var api_domain = 'http://localhost:52118/';
+var api_domain = 'http://kxny.learn-api.milisx.com/';
 
 function ajaxUrl(){ //导航列表url
-    var url = api_domain + 'api/content/pc/';
+    var url = api_domain + 'api/content/v1/';
     return url;
 };
 function lecturerUrls(){ //导航列表url
-    var url = api_domain + 'api/lecturer/pc/';
+    var url = api_domain + 'api/lecturer/v1/';
     return url;
 };
 function isLoginUrl(){ //是否登陆
@@ -15,23 +15,23 @@ function isLoginUrl(){ //是否登陆
     return url;
 };
 function courseUrl(){ //课件
-    var url = api_domain + 'api/course/pc/';
+    var url = api_domain + 'api/course/v1/';
     return url;
 };
 function favoritecourseUrl(){ //赞，收藏
-    var url = api_domain + 'api/user/pc/';
+    var url = api_domain + 'api/user/v1/';
     return url;
 };
 function practiseUrl(){ //分项练习
-    var url = api_domain + 'api/practise/pc/';
+    var url = api_domain + 'api/practise/v1/';
     return url;
 };
 function examUrl(){ //考试
-    var url = api_domain + 'api/exam/pc/';
+    var url = api_domain + 'api/exam/v1/';
     return url;
 };
 
 function redisUrl(){ //清除缓存
-    var url = api_domain + 'api/common/pc/';
+    var url = api_domain + 'api/common/v1/';
     return url;
 };
